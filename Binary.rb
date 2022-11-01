e455bf3fa119d4de0793fa26aca61dcae44ec7d5
Valentine <marlosgama@outlook.com>
2022-10-31 21:27:14 -0300

    Update Binary.rb

--- Binary.rb
+++ Binary.rb
@@ -8,10 +8,13 @@
 
 class Binary
 
-  TYPE = {
+  TYPES = {
     :byte => 'c',
+    :sbyte => 'C',
     :short => 's',
+    :ushort => 'S',
     :int => 'i',
+    :uint => 'I',
     :float => 'f',
     # q representa um número de 64 bits, diferentemente
     #de l, que representa um número de 32 bits
@@ -50,7 +53,7 @@ class Binary_Writer < Binary
   def to_s
     unless manual_types?
       @binary = [@formats.bytesize] + @formats.bytes + @binary
-      @pack = "#{TYPE[:short]}#{TYPE[:byte] * @formats.size}#{@pack}"
+      @pack = "#{TYPES[:ushort]}#{TYPES[:byte] * @formats.size}#{@pack}"
     end
     @binary.pack(@pack)
   end
@@ -60,38 +63,41 @@ class Binary_Writer < Binary
   def write_int(value, type)
     type = type.is_a?(Symbol) ? type : auto_int_type(value)
     push(value, type)
-    @formats << TYPE[type] unless manual_types?
+    @formats << TYPES[type] unless manual_types?
   end
-  
+
   def auto_int_type(value)
     if value >= 0 && value <= 255; :byte
+    elsif value >= -128 && value < 0; :sbyte
     elsif value >= -32_767 && value <= 32_767; :short
+    elsif value > 32_767 && value <= 65_535; :ushort
     elsif value >= -2_147_483_647 && value <= 2_147_483_647; :int
+    elsif value > 2_147_483_647 && value <= 4_294_967_295; :uint
     else; :long
     end
   end
 
   def write_float(value)
     push(value, :float)
-    @formats << TYPE[:float] unless manual_types?
+    @formats << TYPES[:float] unless manual_types?
   end
   
   def write_boolean(value)
     push(value ? 1 : 0, :byte)
-    @formats << TYPE[:boolean] unless manual_types?
+    @formats << TYPES[:boolean] unless manual_types?
   end
   
   def write_string(str)
-    push(str.bytesize, :short)
+    push(str.bytesize, :ushort)
     str.each_byte { |c| push(c, :byte) }
-    @formats << TYPE[:string] unless manual_types?
+    @formats << TYPES[:string] unless manual_types?
   end
 
   def write_date(time)
     push(time.year, :short)
     push(time.month, :byte)
     push(time.day, :byte)
-    @formats << TYPE[:date] unless manual_types?
+    @formats << TYPES[:date] unless manual_types?
   end
 
   def manual_types?
@@ -100,7 +106,7 @@ class Binary_Writer < Binary
   
   def push(value, type)
     @binary << value
-    @pack << TYPE[type]
+    @pack << TYPES[type]
   end
   
 end
@@ -119,14 +125,16 @@ class Binary_Reader < Binary
   def read(type = nil)
     type ||= @formats[@index]
     data = case type
-           when TYPE[:byte], :byte; read_byte
-           when TYPE[:boolean], :boolean; read_boolean
-           when TYPE[:short], :short; read_short
-           when TYPE[:float], :float; shift(4, TYPE[:float])
-           when TYPE[:int], :int; shift(4, TYPE[:int])
-           when TYPE[:long], :long; shift(8, TYPE[:long])
-           when TYPE[:string], :string; read_string
-           when TYPE[:date], :date; read_date
+           when TYPES[:byte], :byte, TYPES[:sbyte], :sbyte; read_byte
+           when TYPES[:boolean], :boolean; read_boolean
+           when TYPES[:short], :short; read_short
+           when TYPES[:ushort], :ushort; read_ushort
+           when TYPES[:float], :float; shift(4, TYPES[:float])
+           when TYPES[:int], :int; shift(4, TYPES[:int])
+           when TYPES[:uint], :uint; shift(4, TYPES[:uint])
+           when TYPES[:long], :long; shift(8, TYPES[:long])
+           when TYPES[:string], :string; read_string
+           when TYPES[:date], :date; read_date
            end
     @index += 1
     data
@@ -143,11 +151,15 @@ class Binary_Reader < Binary
   end
 
   def read_short
-    shift(2, TYPE[:short])
+    shift(2, TYPES[:short])
+  end
+
+  def read_ushort
+    shift(2, TYPES[:ushort])
   end
 
   def read_string
-    size = read_short
+    size = read_ushort
     shift(size, "A#{size}")
   end
   
