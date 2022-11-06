79fa8869e7a62b9dbe7f326fb648b63fe647fb40
Valentine <marlosgama@outlook.com>
2022-11-06 20:02:29 -0300

    Update Binary.rb

--- Binary.rb
+++ Binary.rb
@@ -9,8 +9,9 @@
 class Binary
 
   TYPES = {
-    :byte => 'c',
-    :sbyte => 'C',
+    # Integer 8-bit unsigned
+    :byte => 'C',
+    :sbyte => 'c',
     :short => 's',
     :ushort => 'S',
     :int => 'i',
@@ -51,10 +52,7 @@ class Binary_Writer < Binary
   end
   
   def to_s
-    unless manual_types?
-      @binary = [@formats.bytesize] + @formats.bytes + @binary
-      @pack = "#{TYPES[:ushort]}#{TYPES[:byte]}#{@formats.bytesize}#{@pack}"
-    end
+    concat_formats unless manual_types?
     @binary.pack(@pack)
   end
   
@@ -101,6 +99,13 @@ class Binary_Writer < Binary
     @formats << TYPES[:date] unless manual_types?
   end
 
+  def concat_formats
+    # Reduz formatos utilizando RegExp quando muitos dados forem escritos no buffer
+    @formats.gsub!(/(([a-z])(\2)+)/) { |f| "#{f[0]}#{f.size}" } if @formats.bytesize > 50
+    @binary = [@formats.bytesize] + @formats.bytes + @binary
+    @pack = "#{TYPES[:ushort]}#{TYPES[:byte]}#{@formats.bytesize}#{@pack}"
+  end
+
   def manual_types?
     @manual_types
   end
@@ -118,33 +123,40 @@ end
 class Binary_Reader < Binary
 
   def initialize(binary, manual_types = false)
+    # Cria a posição mutável da array, em vez de usar shift no
+    #método unpack, para que a mesma array seja aproveitada,
+    #e não recriada, durante a leitura
+    @b_pos = 0
+    @f_index = 0
     @bytes = binary.is_a?(Binary_Writer) ? binary.to_s.bytes : binary.bytes
     @formats = read_string unless manual_types
-    @index = 0
   end
 
   def read(type = nil)
-    type ||= @formats[@index]
+    type ||= @formats[@f_index]
     data = case type
-           when TYPES[:byte], TYPES[:sbyte], :byte, :sbyte; read_byte
+           when TYPES[:byte], :byte; read_byte
+           when TYPES[:sbyte], :sbyte; unpack(TYPES[:sbyte], 1)
            when TYPES[:boolean], :boolean; read_boolean
            when TYPES[:short], :short; read_short
            when TYPES[:ushort], :ushort; read_ushort
-           when TYPES[:float], :float; shift(TYPES[:float], 4)
-           when TYPES[:int], :int; shift(TYPES[:int], 4)
-           when TYPES[:uint], :uint; shift(TYPES[:uint], 4)
-           when TYPES[:long], :long; shift(TYPES[:long], 8)
+           when TYPES[:float], :float; unpack(TYPES[:float], 4)
+           when TYPES[:int], :int; unpack(TYPES[:int], 4)
+           when TYPES[:uint], :uint; unpack(TYPES[:uint], 4)
+           when TYPES[:long], :long; unpack(TYPES[:long], 8)
            when TYPES[:string], :string; read_string
            when TYPES[:date], :date; read_date
            end
-    @index += 1
+    @f_index += 1
     data
   end
 
   private
   
   def read_byte
-    @bytes.shift
+    result = @bytes[@b_pos]
+    @b_pos += 1
+    result
   end
   
   def read_boolean
@@ -152,24 +164,26 @@ class Binary_Reader < Binary
   end
 
   def read_short
-    shift(TYPES[:short], 2)
+    unpack(TYPES[:short], 2)
   end
 
   def read_ushort
-    shift(TYPES[:ushort], 2)
+    unpack(TYPES[:ushort], 2)
   end
 
   def read_string
-    bytesize = read_ushort
-    shift("A#{bytesize}", bytesize)
+    size = read_ushort
+    unpack("A#{size}", size)
   end
   
   def read_date
     Time.new(read_short, read_byte, read_byte)
   end
 
-  def shift(format, n)
-    @bytes.shift(n).pack('C*').unpack1(format)
+  def unpack(format, size)
+    result = @bytes[@b_pos, size].pack('C*').unpack1(format)
+    @b_pos += size
+    result
   end
   
 end
